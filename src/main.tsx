696ea7c508392b6e90cf1e7cd42f8fc1a961deb6
Tung Ha <tung.tan.ha@gmail.com>
2026-09-02 19:02:31 +0930

    Fix: correctly instantiate router from src/router.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
-import { router } from "./router";
+import * as routerModule from "./router";
 import "./styles.css";
+
+// Support either createRouter() factory or pre-instantiated router export
+const router =
+  typeof routerModule.createRouter === "function"
+    ? routerModule.createRouter()
+    : routerModule.router || routerModule.default;
 
 const rootElement = document.getElementById("root");
 if (rootElement && !rootElement.innerHTML) {
